376830f21e79c4a51c0ea3ddf925ae553b9296f0
Christian Düfel <christian.duefel@sap.com>
2019-12-09 17:01:10 +0100

    Add skill-mode

--- run.sh
+++ run.sh
@@ -8,12 +8,19 @@ if [[ "$EUID" -ne 0 ]]
 fi
 
 promode=false
+skillmode=false
+numberofprocess=1
 
 while (( "$#" )); do
   case "$1" in
     -p|--pro)
       promode=true
       shift 2
+      ;;
+    -s|--skill)
+      skillmode=true
+      numberofprocess=3
+      shift
       ;;
     --) # end argument parsing
       shift
@@ -28,14 +35,6 @@ while (( "$#" )); do
       ;;
   esac
 done
-
-
-function toArray {
-    SAVEIFS=$IFS   # Save current IFS
-    IFS=$'\n'      # Change IFS to new line
-    r=($1) # split to array $names
-    IFS=${SAVEIFS}   # Restore IFS
-}
 
 function intro() {
     cat << "EOF"
@@ -53,6 +52,8 @@ function intro() {
                                \$$    $$
                                 \$$$$$$
  v0.2
+
+
 EOF
     if [[ "$promode" = true ]]; then
         echo "with pro-mode enabled"
@@ -60,6 +61,15 @@ EOF
         echo "you can enable pro-mode (incl. kernel threads) by starting with -p or --pro"
     fi
 
+    echo
+
+    if [[ "$skillmode" = true ]]; then
+        echo "with skill-mode enabled"
+    else
+        echo "you can enable skill-mode (select on out of 3) by starting with -s or --skill"
+    fi
+
+    echo
 
     read -p "Press Enter"
     # clear screen
@@ -70,7 +80,8 @@ function rules() {
     cat << "EOF"
 THE RULES
 
-Prepare a drink
+1. Prepare a drink
+2. The goal is to kill a process without killing the machine.
 EOF
     read -p "Press Enter to start."
     # clear screen
@@ -93,35 +104,61 @@ while true; do
 
     readarray -t r <<<"$processes"
 
-    i=$(( $RANDOM % ${#r[@]} ))
+    result=()
 
-    pid=$(echo ${r[$i]} | awk '{print $2}')
-    process=$(echo ${r[$i]} | awk '{for (i=11; i<NF; i++) printf $i " "; print $NF}')
+    for (( i = 0; i < ${numberofprocess}; ++i )); do
+        rand=$(( $RANDOM % ${#r[@]} ))
+        pid=$(echo ${r[$rand]} | awk '{print $2}')
+        process=$(echo ${r[$rand]} | awk '{for (i=11; i<NF; i++) printf $i " "; print $NF}')
 
-    echo "The following process has been selected"
-    echo "$pid $process"
+        result[$i]="${pid} '${process}'"
+    done
 
-    read -p "Use -15 (SIGTERM) else -9? (Y/n)" answer
-    case ${answer:0:1} in
-    y|Y )
-        if ! kill -15 ${pid}; then
-            echo "Process was already dead. Drink!"
-        fi
-    ;;
-    * )
-        if ! kill -9 ${pid}; then
-            echo "Process was already dead. Drink!"
-        fi
-    ;;
-    esac
+    if [[ "$skillmode" = false ]]; then
+        echo "The following process has been selected"
+        echo "$pid $process"
+
+        read -p "Kill it with -9? (Y/n)" answer
+        case ${answer:0:1} in
+        n|N )
+            echo "You skipped. Drink!"
+        ;;
+        * )
+            if ! kill -9 ${pid}; then
+                echo "Process was already dead. Drink!"
+            fi
+        ;;
+        esac
+    else
+        echo "Select one of the following process"
+        for i in "${!result[@]}"; do
+            printf "%s\t%s\n" "$i" "${result[$i]}"
+        done
+
+        read -p "Which process to kill? (0-2)" answer
+        case ${answer} in
+        0|1|2 )
+            pid=$(echo ${result[${answer}]} | awk '{print $1}')
+
+            if ! kill -9 ${pid}; then
+                echo "Process was already dead. Drink!"
+            fi
+        ;;
+        * )
+            echo "Wrong input. Drink!"
+        ;;
+        esac
+    fi
 
     echo "Waiting 5s for the process to react".
 
     sleep 5
 
-    if kill -0 ${pid} > /dev/null 2>&1;; then
+    if kill -0 ${pid} > /dev/null 2>&1; then
         echo "Process did not die. Drink!"
+    else
+        echo "SUCCESS you killed ${pid}."
     fi
 
-    read -p "Press enter to continue with next round"
+    read -p "Press enter to continue with next person."
 done
